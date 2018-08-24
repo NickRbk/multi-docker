e36c2e08f29e863eefcd1f19f24a1419ca241c02
Nick Rybak <nrybak94@gmail.com>
2018-08-24 22:42:58 +0300

    initial commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redisHosts: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT,
+    pgUser: process.env.PGUSER,
+    pgHosts: process.env.PGHOST,
+    pgDatabase: process.env.PGDATABASE,
+    pgPassword: process.env.PGPASSWORD,
+    pgPort: process.env.PGPORT
+};
---
+++ worker/index.js
@@ -0,0 +1,19 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+    host: keys.redisHosts,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+const sub = redisClient.duplicate();
+
+function fib(i) {
+    if (i < 2) return 1;
+    return fib(i - 1) + fib(i - 2);
+}
+
+sub.on('message', (channel, message) => {
+    redisClient.hset('values', message, fib(parseInt(message)))
+})
+sub.subscribe('insert');
